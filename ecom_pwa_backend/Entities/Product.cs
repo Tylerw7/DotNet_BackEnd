8f3cb87f9b12908a204742316cf58fc442f3e3d3
Tylerw7 <t4wettach@gmail.com>
2025-11-25 21:37:44 -0500

    first commit

---
+++ ecom_pwa_backend/Entities/Product.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecom_pwa_backend.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+    }
+}
