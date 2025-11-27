1052fa7f0a0002e3c0a8ce1ae1eaf9683ddb4d89
Tylerw7 <t4wettach@gmail.com>
2025-11-27 18:57:03 -0500

    updated Product class

--- ecom_pwa_backend/Entities/Product.cs
+++ ecom_pwa_backend/Entities/Product.cs
@@ -9,17 +9,17 @@ namespace ecom_pwa_backend.Entities
     {
         public int Id { get; set; }
 
-        public string? Name { get; set; }
+        public required string Name { get; set; }
 
-        public string? Description { get; set; }
+        public required string Description { get; set; }
 
         public long Price { get; set; }
 
-        public string? PictureUrl { get; set; }
+        public required string PictureUrl { get; set; }
 
-        public string? Type { get; set; }
+        public required string Type { get; set; }
 
-        public string? Brand { get; set; }
+        public required string Brand { get; set; }
         public int QuantityInStock { get; set; }
     }
 }
