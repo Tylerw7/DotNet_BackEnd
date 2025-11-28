273d1117d2f138ff4e905eb0516f7cbee9939080
Tylerw7 <t4wettach@gmail.com>
2025-11-27 19:31:09 -0500

    added dbcontext for Product class

--- ecom_pwa_backend/Program.cs
+++ ecom_pwa_backend/Program.cs
@@ -1,7 +1,14 @@
+using ecom_pwa_backend.data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
